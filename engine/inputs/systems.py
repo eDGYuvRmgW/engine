7cfea337bb6e9e6d726f321bfe9976a4a908443d
Balaji Veeramani <bveeramani@berkeley.edu>
2021-01-03 18:05:43 -0800

    Appease pydocstyle

--- engine/inputs/systems.py
+++ engine/inputs/systems.py
@@ -1,3 +1,4 @@
+"""Implements the `InputSystem` class."""
 from engine.system import PipelinedSystem
 
 from .key import KeyboardHandlingSystem
@@ -6,6 +7,8 @@ __all__ = ["InputSystem"]
 
 
 class InputSystem(PipelinedSystem):
+    """A system that checks for user input."""
 
     def __init__(self):
+        """Construct and pipeline the systems needed to check for user input."""
         super().__init__([KeyboardHandlingSystem()])
