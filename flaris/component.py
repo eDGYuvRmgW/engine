9ec69819fa27b68f79d98cfa0f0473f67df9a90a
Balaji Veeramani <bveeramani@berkeley.edu>
2021-03-28 18:39:45 -0700

    Update component.py

--- flaris/component.py
+++ flaris/component.py
@@ -12,24 +12,16 @@ __all__ = ["Component", "ComponentError"]
 class Component:  # pylint: disable=too-few-public-methods
     """Raw data for one aspect of an entity."""
 
-    def __init__(self):
-        """Initialize instance attributes."""
-        self._entity = None
-
     @property
     def entity(self) -> Entity:
         """Return the entity that this component is attached to."""
         if not hasattr(self, "_entity"):
-            raise AttributeError("Cannot attach component before"
-                                 "Component.__init__() call.")
+            self._entity = None
         return self._entity
 
     @entity.setter
     def entity(self, value) -> None:
         """Set the entity that this component is attached to."""
-        if not hasattr(self, "_entity"):
-            raise AttributeError("Cannot attach component before"
-                                 "Component.__init__() call.")
         self._entity = value
 
 
